bdb041fedb6498e1e76e9c86a0eae520c75b0684
Adam Siegel <tobioatom@gmail.com>
2011-04-26 02:03:33 -0700

    1st commit

---
+++ js/popup.js
@@ -0,0 +1,43 @@
+var encodeBase = function(inString, base){
+	var output;
+	base = parseInt(base);
+	if(base<63)
+		for (var i = 0; i < inString.length; i++) 
+			output += ' ' + inString.charCodeAt(i).toString(base);
+	else if (base == 64)
+		output = btoa(inString);
+	else
+		output = inString;
+	return output;
+}
+
+var decodeBase = function(inString,base){
+	var output;
+	base = parseInt(base);
+	if(base<63){
+		var baseArray = base.split(" ")
+		for (var i = 0; i < baseArray.length; i ++){
+			output += String.fromCharCode(parseInt(baseArray[i],base).toString(10));
+			}
+	}else if (base == 64){
+		output = atob(inString);
+	}else{
+		output = inString;
+	}
+	return output;
+}
+
+$(function() {
+	$("textarea").keyup(function(e){
+		$(this).addClass("selected")
+		e.preventDefault();
+		var pushVal = decodeBase($(this).val(),$(this).attr("rel"));
+		$("textarea:not(.selected)").each(function (index, item) {
+			alert($(item).attr("id"))
+			$(item).val(encodeBase(pushVal,$(item).attr("rel")))
+		});
+		
+		//changes image
+		$(".b64-imageOutput").attr("src","data:image/png;base64," + $("#b64-base64").val());		
+	});
+});
