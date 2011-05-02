a263ac2c2de25f5a956c391eb83c40406236b9d6
Adam Siegel <tobioatom@gmail.com>
2011-05-02 00:44:25 -0700

    adding in lovely new changes

---
+++ js/content.js
@@ -0,0 +1,12 @@
+var getSText = function(){
+  var t = '';
+  if(window.getSelection){
+    t = window.getSelection();
+  return t;
+}
+selectedText = ''
+$(function() {
+  $('body').mouseup(function(){
+    selectedText = getSText();
+  });
+ });
