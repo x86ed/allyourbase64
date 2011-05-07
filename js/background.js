fa1fff52a2c73e97c3a6f5f8f013635cc18a977c
tobioatom <tobioatom@localhost>
2011-05-07 07:04:54 +0000

    catching 2nd message

--- js/background.js
+++ js/background.js
@@ -1,12 +1,12 @@
-var selectNotify = function(inString){
+var selectNotify = function(inArray){
  var notification = webkitNotifications.createNotification(
       'icons/icon-48.png',  // icon url - can be relative
       'you have selected',  // notification title
-      inString  // notification body text
+      inArray.value  // notification body text
     );
     notification.ondisplay = function(){
       chrome.browserAction.setBadgeBackgroundColor({color: [218 , 17 , 2 , 255]});
-      chrome.browserAction.setBadgeText({text:"str"});
+      chrome.browserAction.setBadgeText({text:inArray.badge});
       chrome.browserAction.setPopup({popup: 'popup.html'});
     }
     notification.show();
@@ -18,7 +18,7 @@ $(function() {
      if(port.name == "selectedText"){
        port.onMessage.addListener(function(msg) {
          if(msg.value.length && oldVal != msg.value)
-           selectNotify(msg.value);
+           selectNotify(msg);
            oldVal = msg.value;
        });
      }
