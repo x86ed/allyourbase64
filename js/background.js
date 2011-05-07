10adc93d8aa1ed96f1c63238ad40b1136a0af54e
tobioatom <tobioatom@localhost>
2011-05-07 01:54:58 +0000

    adding listener

--- js/background.js
+++ js/background.js
@@ -5,4 +5,16 @@ var selectNotify = function(string){
       selectedText  // notification body text
     );
     notification.show();
-}
+}$(function() {
+	chrome.extension.onConnect.addListener(function(port) {
+    if(port.name == " ")
+    port.onMessage.addListener(function(msg) {
+      if (msg.joke == "Knock knock")
+        port.postMessage({question: "Who's there?"});
+      else if (msg.answer == "Madame")
+        port.postMessage({question: "Madame who?"});
+      else if (msg.answer == "Madame... Bovary")
+        port.postMessage({question: "I don't get it."});
+    });
+  });
+});
