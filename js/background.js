f6c8dd292b01cf015096be87c6e4724e1618da9a
tobioatom <tobioatom@localhost>
2011-05-07 01:58:37 +0000

    trying to make alerts work

--- js/background.js
+++ js/background.js
@@ -5,16 +5,15 @@ var selectNotify = function(string){
       selectedText  // notification body text
     );
     notification.show();
-}$(function() {
+    return "SelectNotify()";
+}
+$(function() {
 	chrome.extension.onConnect.addListener(function(port) {
-    if(port.name == " ")
-    port.onMessage.addListener(function(msg) {
-      if (msg.joke == "Knock knock")
-        port.postMessage({question: "Who's there?"});
-      else if (msg.answer == "Madame")
-        port.postMessage({question: "Madame who?"});
-      else if (msg.answer == "Madame... Bovary")
-        port.postMessage({question: "I don't get it."});
-    });
+     if(port.name == "selectedText"){
+       port.onMessage.addListener(function(msg) {
+         if (msg.value)
+           selectNotify(msg.value); 
+       });
+     }
   });
 });
