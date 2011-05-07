270b57f8db272cdac728716ad725cbd9c44dcca5
tobioatom <tobioatom@localhost>
2011-05-07 01:23:26 +0000

    adding messaging to background.js

--- js/content.js
+++ js/content.js
@@ -8,5 +8,7 @@ selectedText = ''
 $(function() {
   $('body').mouseup(function(){
     selectedText = getSText();
+    port = chrome.extension.connect({name: "selectedText"});
+    port.postMessage({value: selectedText});
   });
  });
