3b320a891c64a2bdb2cb68868f9c5054126a783e
tobioatom <tobioatom@localhost>
2011-05-07 01:28:25 +0000

    added notification

--- js/content.js
+++ js/content.js
@@ -10,5 +10,10 @@ $(function() {
     selectedText = getSText();
     port = chrome.extension.connect({name: "selectedText"});
     port.postMessage({value: selectedText});
+    var notification = webkitNotifications.createNotification(
+    '48.png',  // icon url - can be relative
+    'you have selected',  // notification title
+    selectedText  // notification body text
+    );
   });
  });
