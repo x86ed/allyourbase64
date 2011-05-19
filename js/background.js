082a58b3d9790eb07cc693111d8093f5113dfa4f
tobioatom <tobioatom@localhost>
2011-05-19 22:05:48 +0000

    fixing diff issues

--- js/background.js
+++ js/background.js
@@ -58,12 +58,7 @@ function encodeImage(info) {
         BinaryAjax(
         url,
         function(oHTTP) {
-<<<<<<< yours
             console.log(oHTTP.binaryResponse.getBytesAt(0,oHTTP.binaryResponse.getLength());
-=======
-            console.log(oHTTP.binaryResponse.toBase64());
->>>>>>> theirs
-            
         }
     )
 		  typeVal = '#bs-string';
