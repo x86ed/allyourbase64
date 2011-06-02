3ffa3a187198681ebc22788690cfc35b0be57aeb
tobioatom <tobioatom@localhost>
2011-06-02 00:54:48 +0000

    adding .js-orange class

--- js/popup.js
+++ js/popup.js
@@ -154,6 +154,7 @@ $(function() {
     var fieldVal= encodeBase($("#"+ thisID).val(),$("#" + thisID).attr('rel'));
     $('body').append('<div id="bs-floatOver" style="top:' + $thisOffset.top  + 'px;left:' + $thisOffset.left  + 'px" ><label for="bs-floatField">' + thisID.replace('bs-','') + '<!--<span class="bs-button"><span class="bs-clip" id="bs-clipStr" rel="bs-string"></span></span>--><span class="bs-button"><span class="bs-maximize bs-min" rel="' + thisID + '"></span></span></label><textarea id="bs-floatField" rel="99" rows="33">' + fieldVal  + '</textarea></div>');
    $('body').css('height',$('body').height());
+   $('.js-orange').css('height',$('body').height());
    $("div.bs-wordBubble").fadeOut();
    $(".js-bs-mutable").fadeOut();
    $("#bs-floatOver").animate({ top: "10px", left:0, width: $('body').width() - 4 +"px", height: $('body').height() -4 +"px" },200,function(){
